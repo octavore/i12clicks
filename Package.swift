91928dd50b85aab92bdf8ace6b8749b643bdfd39
tp <tp@octavore.com>
2026-07-18 14:27:43 -0400

    improve: Use clickhouse icon in menubar.

--- Package.swift
+++ Package.swift
@@ -7,7 +7,8 @@ let package = Package(
     targets: [
         .executableTarget(
             name: "ClickHouseApp",
-            path: "Sources/ClickHouseApp"
+            path: "Sources/ClickHouseApp",
+            resources: [.copy("Resources/clickhouse.svg")]
         ),
     ]
 )
--- Sources/ClickHouseApp/ClickHouseAppApp.swift
+++ Sources/ClickHouseApp/ClickHouseAppApp.swift
@@ -3,17 +3,31 @@ import SwiftUI
 @main
 struct ClickHouseApp: App {
     @StateObject private var store = InstanceStore()
+    @NSApplicationDelegateAdaptor(AppDelegate.self) private var appDelegate
+
+    private static let menuBarIcon: NSImage = {
+        let url = Bundle.module.url(forResource: "clickhouse", withExtension: "svg")!
+        let image = NSImage(contentsOf: url)!
+        let ratio = image.size.height / image.size.width
+        image.size.height = 14
+        image.size.width = 14 / ratio
+        image.isTemplate = true
+        return image
+    }()
 
     var body: some Scene {
-        MenuBarExtra("ClickHouse", systemImage: "cylinder.split.1x2") {
+        MenuBarExtra {
             StatusMenuView()
                 .environmentObject(store)
+        } label: {
+            Image(nsImage: Self.menuBarIcon)
         }
-        .menuBarExtraStyle(.window)
+        .menuBarExtraStyle(.menu)
 
         Window("ClickHouse", id: "main") {
             MainWindowView()
                 .environmentObject(store)
+                .onAppear { appDelegate.store = store }
         }
 
         Settings {
